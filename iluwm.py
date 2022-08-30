9af055af596689d778f61f4e7e457bba9e3a3f58
Prince Solanki <87561928+Anonymousagent47@users.noreply.github.com>
2022-08-30 14:25:31 +0530

    Update iluwm.py

--- iluwm.py
+++ iluwm.py
@@ -1,57 +1,32 @@
 from turtle import *
-
 import turtle as tur
-  
 
 turt = tur.Turtle()
 tur.title("Proposal")
 
-  
-
 def curve():
     for i in range(200):
-  
-        # Defining step by step curve motion
         turt.right(1)
         turt.forward(1)
-  
 
 def heart():
-  
-    # Set the fill color to red
     turt.fillcolor('red')
-  
-    # Start filling the color
     turt.begin_fill()
-  
     turt.left(140)
     turt.forward(113)
-  
     curve()
     turt.left(120)
-  
-    # Draw the right curve
     curve()
-  
     turt.forward(112)
     turt.end_fill()
   
 def txt():
- 
     turt.up()
     turt.setpos(-68, 95)
     turt.down()
     turt.color('cyan')
-    turt.write(" I Love You ", font=(
-      "Times New Roman", 16, "bold"))
-  
-  
-# Draw a heart
+    turt.write(" I Love You ", font=("Times New Roman", 16, "bold")) #HERE, YOU CAN ADD SOMEONE'S NAME ALSO
 heart()
-  
-# Write text
 txt()
-  
-# To hide turtle
 tur.ht()
 tur.done()
