706d82a05a2140228094699469c978b56deef70a
Prince Solanki <87561928+Anonymousagent47@users.noreply.github.com>
2022-08-30 14:21:03 +0530

    Add files via upload
    This Code is to draw Heart And Type I Love You inside..
    This is very helpful to impress whom you love/like..
    
    Enjoy!!!!!!!!!

---
+++ heart.py
@@ -0,0 +1,15 @@
+#pip instal tuttle
+import turtle
+screen=turtle.Screen()
+h = turtle.Turtle()
+h.color('red')
+h.begin_fill()
+h.pensize(5)
+h.left(50)
+h.forward(133)
+h.circle(50,200)
+h.right(140)
+h.circle(50,200)
+h.forward(133)
+h.end_fill()
+turtle.done()
